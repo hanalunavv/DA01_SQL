4ecb694f77507c785740cf549fd7bc66d7ef4b63
hanaaa <157679723+hanalunavv@users.noreply.github.com>
2024-02-19 00:12:25 +0700

    practice2_day5.sql

--- practice2_day5.sql
+++ practice2_day5.sql
@@ -1,4 +1,22 @@
 --EX1
-select distinct CITY from STATION
+select distinct CITY
+from STATION
 where ID %2=0
 --EX2
+select count(CITY) - count(distinct CITY) from STATION
+--EX3
+select ceil(avg(salary) - avg(replace(salary, '0', '')))
+from EMPLOYEES
+--EX4
+SELECT 
+ROUND((CAST(SUM(item_count*order_occurrences)/SUM(order_occurrences) as Decimal)),1) as mean
+FROM 
+items_per_order
+--EX5
+SELECT candidate_id
+FROM candidates
+where skill in ('Python','Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+having count (DISTINCT skill) = 3
+ORDER BY candidate_id 
+--EX6
