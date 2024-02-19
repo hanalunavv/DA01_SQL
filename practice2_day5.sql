d88356b6e9cca9028680e5993297787dbd19cd06
hanaaa <157679723+hanalunavv@users.noreply.github.com>
2024-02-20 01:34:34 +0700

    practice2_day5.sql

--- practice2_day5.sql
+++ practice2_day5.sql
@@ -21,9 +21,42 @@ having count (DISTINCT skill) = 3
 ORDER BY candidate_id 
 --EX6
 SELECT user_id, 
-EXTRACT(day FROM MAX(post_date)- min(post_date))  AS days_between
+EXTRACT(day FROM max(post_date)- min(post_date))  AS days_between
 FROM posts
 WHERE post_date BETWEEN '01/01/2021' AND '12/31/2021'
 GROUP BY user_id
 having COUNT(DISTINCT post_id)>=2
 --EX7
+SELECT 
+card_name,
+MAX(issued_amount) - MIN(issued_amount) as different
+FROM monthly_cards_issued
+GROUP BY card_name 
+ORDER BY card_name DESC
+--EX8
+SELECT 
+manufacturer,
+Abs(SUM(cogs - total_sales)) as total_loss,
+COUNT((cogs - total_sales)>0) as drug_count
+FROM pharmacy_sales
+WHERE (cogs - total_sales)>0 /* where cogs>total_sales*/
+GROUP BY manufacturer
+ORDER BY total_loss DESC
+/* cogs = cost of goods sold */
+--EX9
+select *
+from Cinema
+where not(description like 'boring' ) and  id %2!=0
+order by rating DESC
+--EX10
+select teacher_id,
+count(distinct subject_id) as cnt
+from Teacher
+group by teacher_id
+--EX11
+select user_id,
+count(distinct follower_id) as followers_count
+from Followers
+group by  user_id
+order by user_id\
+--EX12
