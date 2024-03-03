7fb9a1cc49f514cc7a3b1f7ec9d8b5e1e770822a
hanaaa <157679723+hanalunavv@users.noreply.github.com>
2024-03-03 23:17:47 +0700

    practice3_day7.sql

--- practice3_day7.sql
+++ practice3_day7.sql
@@ -4,3 +4,9 @@ From STUDENTS
 Where Marks>75
 Order By Right(name,3), ID ASC
 --EX2
+select 
+user_id,
+concat(upper(substring(name,1,1)),lower(substring(name,2))) as  name
+from Users
+order by user_id
+--Ex3
